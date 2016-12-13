f3b700883d3ecf170989d762111f01584cb63ab1
Krisztian Pinter <kpinter@brainsum.com>
2016-12-13 12:38:44 +0100

    HOME | responsive iconline section

--- inc/section-iconline.php
+++ inc/section-iconline.php
@@ -4,27 +4,27 @@
 			<div class="col-xs-12">
 				<h2>spinemed wirkt so:</h2>
 			</div>
-			<div class="col-xs-6 col-md-2 icon-holder odd">
+			<div class="col-xs-6 col-sm-4 col-md-2 icon-holder odd">
 				<img src="img/ikon-schmerzfrei.png" alt="ikon-schmerzfrei">
 				<p>schmerzfrei</p>
 			</div>
-			<div class="col-xs-6 col-md-2 icon-holder even">
+			<div class="col-xs-6 col-sm-4 col-md-2 icon-holder even">
 				<img src="img/ikon-sicher.png" alt="ikon-sicher">
 				<p>sicher</p>
 			</div>
-			<div class="col-xs-6 col-md-2 icon-holder odd">
+			<div class="col-xs-6 col-sm-4 col-md-2 icon-holder odd">
 				<img src="img/ikon-nebenwirkung.png" alt="ikon-nebenwirkung">
 				<p>ohne nebenwirkungen</p>
 			</div>
-			<div class="col-xs-6 col-md-2 icon-holder even">
+			<div class="col-xs-6 col-sm-4 col-md-2 icon-holder even">
 				<img src="img/ikon-spritzen.png" alt="ikon-spritzen">
 				<p>ohne spritzen</p>
 			</div>
-			<div class="col-xs-6 col-md-2 icon-holder odd">
+			<div class="col-xs-6 col-sm-4 col-md-2 icon-holder odd">
 				<img src="img/ikon-medikamente.png" alt="ikon-medikamente">
 				<p>ohne medikamente</p>
 			</div>
-			<div class="col-xs-6 col-md-2 icon-holder even">
+			<div class="col-xs-6 col-sm-4 col-md-2 icon-holder even">
 				<img src="img/ikon-operation.png" alt="ikon-operation">
 				<p>ohne operation</p>
 			</div>

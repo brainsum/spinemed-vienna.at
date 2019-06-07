fce5b2d41813c8defa4374e0b216c310c08349e6
Peter Ponya <pedro@brainsum.com>
2019-06-07 15:34:38 +0200

    redirect to https on med-rent domain

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
-
 <?php
 
+if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") && preg_match("/med-rent.de$/", $_SERVER['HTTP_HOST'])) {
+  $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  header('HTTP/1.1 301 Moved Permanently');
+  header('Location: ' . $location);
+  exit;
+}
 /* PAGE HEAD
 ************/
 include('head.php');

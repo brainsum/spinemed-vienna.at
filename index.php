8b59f76ba04a25276cdf4e370a0479fbc1aed21e
Peter Ponya <pedro@brainsum.com>
2019-06-07 15:41:27 +0200

    redirect to https and www always

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 if ((empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") && preg_match("/med-rent.de$/", $_SERVER['HTTP_HOST'])) {
-  $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  $location = 'https://' . 'www.med-rent.de' . $_SERVER['REQUEST_URI'];
   header('HTTP/1.1 301 Moved Permanently');
   header('Location: ' . $location);
   exit;

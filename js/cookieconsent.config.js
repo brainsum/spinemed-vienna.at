55bffa46bddcbf0ce8cbd34cabc3ba8090d81ca2
Krisztian Pinter <kpinter@brainsum.com>
2019-09-05 16:01:04 +0200

    MED-4 | Added DE translations to cookie consent.

--- js/cookieconsent.config.js
+++ js/cookieconsent.config.js
@@ -12,15 +12,15 @@ window.CookieConsent.init({
         current: 'de',
         locale: {
             de: {
-                barMainText: 'This website uses cookies to ensure you get the best experience on our website.',
-                barLinkSetting: 'Cookie Settings',
-                barBtnAcceptAll: 'Accept all cookies',
+                barMainText: 'Diese Website verwendet Cookies – nähere Informationen dazu und zu Ihren Rechten als Benutzer finden Sie in unserer Datenschutzerklärung am Ende der Seite. Klicken Sie auf „Akzeptiere alle Cookies“, um Cookies zu akzeptieren und direkt unsere Website besuchen zu können.',
+                barLinkSetting: 'Cookie-Einstellungen',
+                barBtnAcceptAll: 'Akzeptiere alle Cookies',
                 modalMainTitle: 'Cookie settings',
-                modalMainText: 'Cookies are small piece of data sent from a website and stored on the user\'s computer by the user\'s web browser while the user is browsing. Your browser stores each message in a small file, called cookie. When you request another page from the server, your browser sends the cookie back to the server. Cookies were designed to be a reliable mechanism for websites to remember information or to record the user\'s browsing activity.',
-                modalBtnSave: 'Save current settings',
-                modalBtnAcceptAll: 'Accept all cookies and close',
-                modalAffectedSolutions: 'Affected solutions:',
-                learnMore: 'Learn More',
+                modalMainText: 'Cookies sind kleine Daten, die von einer Website gesendet und vom Webbrowser des Benutzers auf dem Computer des Benutzers gespeichert werden, während der Benutzer surft. Ihr Browser speichert jede Nachricht in einer kleinen Datei, die als Cookie bezeichnet wird. Wenn Sie eine andere Seite vom Server anfordern, sendet Ihr Browser das Cookie zurück an den Server. Cookies wurden als zuverlässiger Mechanismus für Websites entwickelt, um Informationen zu speichern oder die Surfaktivität des Benutzers aufzuzeichnen.',
+                modalBtnSave: 'Aktuelle Einstellungen speichern',
+                modalBtnAcceptAll: 'Alle Cookies akzeptieren und schließen',
+                modalAffectedSolutions: 'Betroffene Lösungen:',
+                learnMore: 'Mehr erfahren',
                 on: 'On',
                 off: 'Off',
             }
@@ -34,8 +34,8 @@ window.CookieConsent.init({
             language: {
                 locale: {
                     de: {
-                        name: 'Strictly Necessary Cookies',
-                        description: 'These are cookies that are required for the operation of our website. They include, for example, cookies that enable you to log into secure areas of our website. Statistics cookies allows us to recognise and count the number of visitors and to see how visitors move around our website when they are using it. This helps us to improve the way our website works, for example, by ensuring that users are finding what they are looking for easily.',
+                        name: 'Unbedingt notwendige Cookies',
+                        description: 'Unbedingt notwendige Cookies sind kleine Daten, die von einer Website gesendet und vom Webbrowser des Benutzers auf dem Computer des Benutzers gespeichert werden, während der Benutzer surft. Cookies wurden als zuverlässiger Mechanismus für Websites entwickelt, um Informationen zu speichern oder die Surfaktivität des Benutzers aufzuzeichnen.',
                     }
                 }
             }

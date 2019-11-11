41dc0694cd1ce67c3ac392d9944b9c8f07c02c97
Krisztian Pinter <kpinter@brainsum.com>
2019-11-11 14:47:47 +0100

    MED-20 | Complement one missing word to intro section.

--- inc/section-intro.php
+++ inc/section-intro.php
@@ -11,7 +11,7 @@
                 <ul>
                     <li>Sie haben keine Lust auf Spritzen oder gar eine Operation?</li>
                     <li>Sie haben Rückenprobleme und schon vieles probiert, und haben immer noch Schmerzen?</li>
-                    <li>SpineMED® wirkt auf natürliche Weise direkt dort, wo das Problem besteht. Es wird die Ursache der Schmerzen therapiert, und nicht nur die</li>
+                    <li>SpineMED® wirkt auf natürliche Weise direkt dort, wo das Problem besteht. Es wird die Ursache der Schmerzen therapiert, und nicht nur die Symptome.</li>
                     <li>Sie müssen Schmerzen nicht als unabänderliche Tatsache akzeptieren!</li>
                 </ul>
                 <strong>Diese Ärzte sind unsere Partner bei der SpineMED®
